a60a0f4e775fae2888c9489ef33221e9d665b11e
rainaeverson <rain.a.everson@gmail.com>
2023-08-03 21:14:28 -0500

    Commit

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "10969a45ca252efb86c54429c21da9a0"
+weather_api_key = "1e0b253a8927e4b2b112ccb8a4207906"
 
 # Geoapify API Key
 geoapify_key = "b314fcd15c414c8691d18d4a3a5ed7ac"
